c6e10726f2d98a0a28da228aee2db248e3cd2042
fixit-labs <Azure@fixitg.com>
2026-02-24 08:00:51 -0500

    Update 20250105001414_add_status_to_captain_documents.rb

--- db/migrate/20250105001414_add_status_to_captain_documents.rb
+++ db/migrate/20250105001414_add_status_to_captain_documents.rb
@@ -1,6 +1,6 @@
 class AddStatusToCaptainDocuments < ActiveRecord::Migration[7.0]
   def change
+    return unless table_exists?(:captain_documents)
     add_column :captain_documents, :status, :integer, null: false, default: 0
-    add_index :captain_documents, :status
   end
 end
